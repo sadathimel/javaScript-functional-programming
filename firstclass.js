557443361fa99dfb2b260fd06e568b4ae5d7e840
SADAT <sadat.himel@gmail.com>
2020-04-21 01:54:16 +0600

    function can be stored is an Array

--- firstclass.js
+++ firstclass.js
@@ -10,5 +10,9 @@ console.log(sum(4,5))
 console.log(typeof sum)
 
 //2 function can be stored ia an Array
+var arr = []
+  arr.push(add)
+  console.log(arr)
+  console.log(arr[0](5,4))
 
 //3 function can be stored ia an Object
