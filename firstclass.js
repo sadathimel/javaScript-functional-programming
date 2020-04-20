979a8eb7c6c33e69dada550151c8ff52d9e2cd98
SADAT <sadat.himel@gmail.com>
2020-04-21 01:46:27 +0600

    crite firstclass.js functional program

---
+++ firstclass.js
@@ -0,0 +1,11 @@
+// First Class function
+
+function add(a,b){
+  return a + b
+}
+
+//1. function can be stored ia a variable
+var sum = add
+console.log(sum(4,5))
+//2 function can be stored ia an Array
+//3 function can be stored ia an Object
