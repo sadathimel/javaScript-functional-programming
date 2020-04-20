154ae62ce8675856a7caa19026f95d8c8b8426c5
SADAT <sadat.himel@gmail.com>
2020-04-21 01:48:59 +0600

    function can be stored ia a variable

--- firstclass.js
+++ firstclass.js
@@ -7,5 +7,8 @@ function add(a,b){
 //1. function can be stored ia a variable
 var sum = add
 console.log(sum(4,5))
+console.log(typeof sum)
+
 //2 function can be stored ia an Array
+
 //3 function can be stored ia an Object
